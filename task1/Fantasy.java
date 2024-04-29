f9bfe6ac9c434983090264644ba9279d786fb5b5
Kamil <kdnuriev@gmail.com>
2024-04-29 09:37:55 +0400

    finished task 1

--- task1/Fantasy.java
+++ task1/Fantasy.java
@@ -1,13 +1,36 @@
 package midterm.kamil_nuriev_1.task1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fantasy extends AbstractFantasy implements Dream {
     // TODO change studentName to your name
-    private String studentName;
+    private String studentName = "Kamil Nuriev";
 
     public String toString() {
         String result = "";
 
-        // TODO must be implemented
+        result = "Fantasy Object: studentName= " + studentName;
         return result;
+    }
+
+    @Override
+    public String getJustice1() {
+        return "Justice1";
+    }
+
+    @Override
+    public String getTopology2() {
+        return "Topology2";
+    }
+
+    @Override
+    public String methodMicroscope3(String argHelmet5) {
+        return "argHelmet6";
+    }
+
+    @Override
+    public List<String> methodDisease4(String argThermodynamics6) {
+        return new ArrayList<>();
     }
 }
